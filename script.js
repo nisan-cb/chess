e856de1622c856ca0157556704790012df24d65b
Nisan-cohen <76742636+Nisan-cb@users.noreply.github.com>
2022-04-25 15:23:34 +0300

    new commit

--- script.js
+++ script.js
@@ -24,20 +24,18 @@ class BoardData {
     createGroup(color, group) {    // insert piece into group with starting positions
         let k = 0;
         if (color == 'black') k = 7;
-        group["king"] = new King(color); group["king"].setLocation(k, 4);
-        group["queen"] = new Queen(color); group["queen"].setLocation(k, 3);
-        group["rookL"] = new Rook(color); group["rookL"].setLocation(k, 0);
-        group["rookR"] = new Rook(color); group["rookR"].setLocation(k, 7);
-        group["bishopL"] = new Bishop(color); group["bishopL"].setLocation(k, 2);
-        group["bishopR"] = new Bishop(color); group["bishopR"].setLocation(k, 5);
-        group["knightL"] = new Knight(color); group["knightL"].setLocation(k, 1);
-        group["knightR"] = new Knight(color); group["knightR"].setLocation(k, 6);
+        group["king"] = new King(color, k, 4);
+        group["queen"] = new Queen(color, k, 3);
+        group["rookL"] = new Rook(color, k, 0);
+        group["rookR"] = new Rook(color, k, 7);
+        group["bishopL"] = new Bishop(color, k, 2);
+        group["bishopR"] = new Bishop(color, k, 5);
+        group["knightL"] = new Knight(color, k, 1);
+        group["knightR"] = new Knight(color, k, 6);
         k = 1;
         if (color == 'black') k = 6;
-        for (let i = 0; i < 8; i++) {
-            group[`pawn${i}`] = new Pawn(color);
-            group[`pawn${i}`].setLocation(k, i);
-        }
+        for (let i = 0; i < 8; i++)
+            group[`pawn${i}`] = new Pawn(color, k, i);
     }
     placeAllSoldiers() {
         this.placeSoldiers(this.whiteGroup); // place white pices on the board
@@ -45,31 +43,26 @@ class BoardData {
     }
     placeSoldiers(group) { //in matrix & into HTML table
         for (let key in group) {
-            if (group[key] !== undefined) {
-                boardData.insertPiece(group[key]);// into matrix
-                group[key].display(table);// into HTML table
-            }
+            boardData.insertPiece(group[key]);// into matrix
+            group[key].display(table);// into HTML table
         }
     }
-    displayStepsOf(currentPiece) {
+    displayPosibalMoves(currentPiece) {
         for (let step of currentPiece.optionalSteps) {
-            table.rows[step[0]].cells[step[1]].classList.add("checked");
+            table.rows[step[0]].cells[step[1]].classList.add("step");
             table.rows[step[0]].cells[step[1]].addEventListener('click', this.validMovesHandler);
         }
-        if (currentPiece !== undefined)
-            table.rows[currentPiece.r].cells[currentPiece.c].classList.add('checked');
+        table.rows[currentPiece.r].cells[currentPiece.c].classList.add('selected');
     }
     validMovesHandler(e) {
         e.stopPropagation();
         let cell = e.target;
         let new_c = cell.cellIndex, new_r = cell.parentNode.rowIndex; // get new indexes
-        boardData.matrix[currentPiece.r][currentPiece.c] = undefined; // free the prev position
+        boardData.cleanValidSteps();
+        boardData.freePosition(currentPiece); // in matrix and in table element
         boardData.matrix[new_r][new_c] = currentPiece; // set into the matrix new position
-        boardData.table.rows[currentPiece.r].cells[currentPiece.c].classList.remove('checked'); // 
-        boardData.table.rows[currentPiece.r].cells[currentPiece.c].innerHTML = ''; // remove piece from prev cell
         currentPiece.setLocation(new_r, new_c); // updet new indexes in the currentPiece
         boardData.table.rows[currentPiece.r].cells[currentPiece.c].appendChild(currentPiece.el);
-        boardData.cleanValidSteps();
         boardData.switchTurn();
         if (currentPiece.constructor.name === 'Pawn') currentPiece.firstStep = false;
         currentPiece = undefined;
@@ -77,43 +70,66 @@ class BoardData {
     cleanValidSteps() { // clean step of prev piece from the board
         if (!currentPiece) return;
         for (let step of currentPiece.optionalSteps) {
-            this.table.rows[step[0]].cells[step[1]].classList.remove('checked');
+            this.table.rows[step[0]].cells[step[1]].classList.remove('step');
             this.table.rows[step[0]].cells[step[1]].removeEventListener('click', this.validMovesHandler);
         }
-        this.table.rows[currentPiece.r].cells[currentPiece.c].classList.remove('checked');
+        this.table.rows[currentPiece.r].cells[currentPiece.c].classList.remove('selected'); // 
     }
     switchTurn() {
         document.getElementById(`${this.turn}-flag`).classList.remove('turn');
         this.turn = (this.turn === 'white') ? 'black' : 'white';
         document.getElementById(`${this.turn}-flag`).classList.add('turn');
-
+    }
+    kill(piece) {
+        this.freePosition(currentPiece);
+        this.freePosition(piece);
+        boardData.cleanValidSteps();
+        currentPiece.setLocation(piece.r, piece.c);
+        currentPiece.display(this.table);
+        currentPiece = undefined;
+    }
+    freePosition(piece) {
+        this.matrix[piece.r][piece.c] = undefined; //remove from matrix
+        this.table.rows[piece.r].cells[piece.c].innerHTML = ''; //remove from table element
     }
 }
 
 class Piece {
-    constructor(color, role) {
+    constructor(color, role, r = 0, c = 0) {
         this.color = color;
         this.src = `./images/${color}_${role}.png`;
         this.el = document.createElement('img');
         this.el.src = this.src;
-        this.r = 0;
-        this.c = 0;
+        this.r = r;
+        this.c = c;
         this.optionalSteps = []; // optional valid steps
         this.el.addEventListener('click', (e) => {
-            if (boardData.turn !== this.color) { alert(`it is ${boardData.turn} turn to play`); return };
-            e.stopPropagation();
-            boardData.cleanValidSteps();
-            currentPiece = this;
-            this.calcOptionalSteps();
-            boardData.displayStepsOf(this);
+            // if (boardData.turn !== this.color) { alert(`it is ${boardData.turn} turn to play`); return };
+            if (this.isInDanger()) {
+                boardData.kill(this);
+            } else {
+                boardData.cleanValidSteps();
+                currentPiece = this;
+                this.calcOptionalSteps();
+                boardData.displayPosibalMoves(this);
+            }
         })
         this.el.addEventListener("mouseover", (e) => {
             this.calcOptionalSteps();
             if (this.optionalSteps.length === 0)
                 this.el.style.cursor = 'not-allowed';
-            else
+            else if (this.isInDanger()) {
+                this.el.style.cursor = 'grab';
+            } else
                 this.el.style.cursor = 'pointer';
         })
+    }
+    isInDanger() {
+        if (!currentPiece) return false;
+        for (const cell of currentPiece.optionalSteps)
+            if (this.r === cell[0] && this.c === cell[1])
+                return true;
+        return false;
     }
     display(table) {
         table.rows[this.r].cells[this.c].appendChild(this.el);
@@ -122,24 +138,28 @@ class Piece {
         this.r = r;
         this.c = c;
     }
-    checkPath(i, j) {
+    calcOptionalSteps() {
+        this.optionalSteps = [];
+        for (let direction of this.directions)
+            this.checkPath(direction[0], direction[1]);
+    }
+    checkPath(i, j) { // i and j indicate direction as up/down/left/right/diagonal...
         let r = this.r + i, c = this.c + j;
         for (; r >= 0 && r < ROWS && c >= 0 && c < COLS; r += i, c += j) {
             let cellData = boardData.getCellData(r, c);
             if (cellData == undefined)
                 this.optionalSteps.push([r, c]);
-            else if (cellData.color === this.color)
-                break;
-            else if (cellData.color !== this.color) {
-                this.optionalSteps.push([r, c]);
+            else { // if cell not empty
+                if (cellData.color !== this.color)
+                    this.optionalSteps.push([r, c]);
                 break;
             }
         }
     }
 }
 class King extends Piece {
-    constructor(color) {
-        super(color, 'king');
+    constructor(color, r = 0, c = 0) {
+        super(color, 'king', r, c);
         this.vector = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
     }
     calcOptionalSteps(e) {
@@ -148,60 +168,38 @@ class King extends Piece {
             let new_r = this.r + step[0], new_c = this.c + step[1];
             if (new_r >= 0 && new_r < 8 && new_c >= 0 && new_c < 8) {
                 let cellData = boardData.getCellData(new_r, new_c);
-                if (cellData == undefined)
+                if (cellData == undefined || cellData.color !== this.color)
                     this.optionalSteps.push([new_r, new_c]);
-                else if (cellData.color !== this.color) {
-                    this.optionalSteps.push([new_r, new_c]);
-                }
             }
         }
-
     }
 }
 class Queen extends Piece {
-    constructor(color) {
-        super(color, 'queen')
+    constructor(color, r = 0, c = 0) {
+        super(color, 'queen', r, c)
         this.directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
-    }
-    calcOptionalSteps() {
-        this.optionalSteps = [];
-        for (let direction of this.directions) {
-            this.checkPath(direction[0], direction[1]);
-        }
     }
 }
 class Rook extends Piece {
-    constructor(color) {
-        super(color, 'rook')
+    constructor(color, r = 0, c = 0) {
+        super(color, 'rook', r, c)
         this.directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    }
-    calcOptionalSteps(e) {
-        this.optionalSteps = [];
-        for (let direction of this.directions) {
-            this.checkPath(direction[0], direction[1]);
-        }
     }
 }
 class Bishop extends Piece {
-    constructor(color) {
-        super(color, 'bishop')
+    constructor(color, r = 0, c = 0) {
+        super(color, 'bishop', r, c)
         this.directions = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
-    }
-    calcOptionalSteps(e) {
-        this.optionalSteps = [];
-        for (let direction of this.directions) {
-            this.checkPath(direction[0], direction[1]);
-        }
     }
 }
 class Knight extends Piece {
-    constructor(color) {
-        super(color, 'knight')
-        this.vector = [[-1, 2], [-1, -2], [1, 2], [1, - 2], [-2, 1], [-2, -1], [2, 1], [2, -1]];
+    constructor(color, r = 0, c = 0) {
+        super(color, 'knight', r, c)
+        this.moves = [[-1, 2], [-1, -2], [1, 2], [1, - 2], [-2, 1], [-2, -1], [2, 1], [2, -1]];
     }
     calcOptionalSteps() {
         this.optionalSteps = [];
-        for (let step of this.vector) {
+        for (let step of this.moves) {
             let new_r = this.r + step[0], new_c = this.c + step[1];
             if (new_r >= 0 && new_r < 8 && new_c >= 0 && new_c < 8) {
                 let cellData = boardData.getCellData(new_r, new_c);
@@ -212,19 +210,20 @@ class Knight extends Piece {
     }
 }
 class Pawn extends Piece {
-    constructor(color) {
-        super(color, 'pawn')
+    constructor(color, r = 0, c = 0) {
+        super(color, 'pawn', r, c)
         this.d = (color === 'black') ? -1 : 1;
         this.firstStep = true;
     }
     calcOptionalSteps() {
         this.optionalSteps = [];
         let new_r = this.r + this.d;
+        if (new_r >= ROWS || new_r < 0) return;
         let cellData = boardData.getCellData(new_r, this.c);
         if (cellData == undefined) {
             this.optionalSteps.push([new_r, this.c]);
             let twoStepsCellData = boardData.getCellData(new_r + this.d, this.c)
-            if (this.firstStep && (twoStepsCellData == undefined || twoStepsCellData.color !== this.color))
+            if (this.firstStep && twoStepsCellData === undefined)
                 this.optionalSteps.push([new_r + this.d, this.c]);
         }
         let upLeftCell = boardData.getCellData(new_r, this.c - 1);
@@ -235,7 +234,6 @@ class Pawn extends Piece {
             this.optionalSteps.push([new_r, this.c + 1]);
     }
 }
-
 
 function createBoard() {
     let areaObj = document.getElementById('board-box');
@@ -268,4 +266,4 @@ window.addEventListener('load', () => {
     init();
 });
 
-//270
+//269
